3b73b1c60eda26bfa2791653230ae878cbd07a35
CrisExpositoSerra <crisexposito18@gmail.com>
2020-02-18 16:18:09 +0100

    nuevo repositorio creado copiado codigo Enzinium(borrado)

---
+++ src/main/java/edu/elsmancs/Address.java
@@ -0,0 +1,55 @@
+package edu.elsmancs;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Address {
+    private PrivateKey SK;
+    private PublicKey PK;
+    private double Balance = 0d;
+    private String Symbol = "EZI";
+
+    public void generateKeyPair(){
+        KeyPair parejaDeclaves = GenSig.generateKeyPair();
+        this.PK = parejaDeclaves.getPublic();
+        this.SK = parejaDeclaves.getPrivate();
+    }
+
+    public PublicKey getPK() {
+        this.PK = PK;
+        return PK;
+    }
+
+    public double getBalance() {
+        this.Balance = Balance;
+        return Balance;
+    }
+
+    public PrivateKey getSK() {
+        this.SK = SK;
+        return SK;
+    }
+
+    public String getSymbol() {
+        this.Symbol = Symbol;
+        return Symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicKey: " + PK.hashCode() + "\nBalance: " + getBalance();
+    }
+
+    public void transferEZI(double enziniums) {
+
+        this.Balance += enziniums;
+    }
+
+    public void send(TokenContract contract, Double enziniums) {
+        if (enziniums <= this.Balance) {
+            contract.payable(getPK(), enziniums);
+            this.Balance -= enziniums;
+        }
+    }
+}
